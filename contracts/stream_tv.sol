189cec4d5e85a565d8d1b59f73acf4fa70166bdc
isaack-mungui <munguiisaack@gmail.com>
2022-03-26 10:50:49 +0300

    Polished code

--- contracts/stream_tv.sol
+++ contracts/stream_tv.sol
@@ -5,9 +5,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract StreamX is ERC20, Ownable, ERC20Burnable {
+
     event tokensBurned(address indexed owner, uint256 amount, string message);
     event tokensMinted(address indexed owner, uint256 amount, string message);
     event additionalTokensMinted(address indexed owner,uint256 amount,string message);
+
 
     constructor() ERC20("StreamX", "STX") {
         _mint(msg.sender, 1000 * 10**decimals());
